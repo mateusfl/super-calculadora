eb64725e61602ba8284e691b442c8d5dc2ecbfcc
mateusfl <mateusfelipern@gmail.com>
2022-10-04 09:02:01 -0300

    anchors consertados

--- media.js
+++ media.js
@@ -1,48 +1,49 @@
-var botao = document.querySelector('#botao')
-    botao.addEventListener('click', calcular)
-    
-    var botaoAdicionar = document.querySelector('#adicionar')
-    botaoAdicionar.addEventListener('click', adicionaNumero)
-    
-    var botaoRemover = document.querySelector('#remover')
-    botaoRemover.addEventListener('click', retiraNumero)
+var botao = document.querySelector("#botao");
+botao.addEventListener("click", calcular);
 
-    var caixaResultado = document.querySelector('#resultado')
+var botaoAdicionar = document.querySelector("#adicionar");
+botaoAdicionar.addEventListener("click", adicionaNumero);
 
-    function adicionaNumero(){
-        var novoInput = document.createElement('input')
-        novoInput.className = 'numeros'
-        novoInput.type = 'number'
-        document.querySelector('.inputs').appendChild(novoInput)
-    }
-    function retiraNumero(){
-        if(document.querySelectorAll('.numeros').length > 2){
-            document.querySelector('.inputs').removeChild(document.querySelector('.inputs').lastChild)
-        }
-        else{
-        }
-    }
+var botaoRemover = document.querySelector("#remover");
+botaoRemover.addEventListener("click", retiraNumero);
 
-    function calcular(){
-        /* cria um array com o tamanho da quantidade de inputs e preenche com undefined */
-        var numeros = new Array(document.querySelectorAll('.numeros').length).fill()
-        var media = 0
+var caixaResultado = document.querySelector("#resultado");
 
-        /* coleta o valor do array de classes numeros, na posição i, transforma em real e guarda no array numeros */
-        for (let i = 0; i < numeros.length; i++) {
-            numeros[i] = parseFloat(document.querySelectorAll('.numeros')[i].value) 
-            media += numeros[i]
-        }
+function adicionaNumero() {
+  var novoInput = document.createElement("input");
+  novoInput.className = "numeros";
+  novoInput.type = "number";
+  document.querySelector(".inputs").appendChild(novoInput);
+}
+function retiraNumero() {
+  if (document.querySelectorAll(".numeros").length > 2) {
+    document
+      .querySelector(".inputs")
+      .removeChild(document.querySelector(".inputs").lastChild);
+  } else {
+  }
+}
 
-        media = media / numeros.length
+function calcular() {
+  /* cria um array com o tamanho da quantidade de inputs e preenche com undefined */
+  var numeros = new Array(document.querySelectorAll(".numeros").length).fill();
+  var media = 0;
 
-        if(Number.isNaN(media) == true){
-            caixaResultado.innerHTML = `<p id="resultado">Erro. Preencha todos os campos.</p>`
-        }
-        else if(Number.isInteger(media) == true){
-            caixaResultado.innerHTML = `<p id="resultado">O resultado é: <b>${media}</b></p>`
-        }
-        else{
-            caixaResultado.innerHTML = `<p id="resultado">O resultado é: <b>${media.toFixed(3)}</b></p>`
-        }
-    }
+  /* coleta o valor do array de classes numeros, na posição i, transforma em real e guarda no array numeros */
+  for (let i = 0; i < numeros.length; i++) {
+    numeros[i] = parseFloat(document.querySelectorAll(".numeros")[i].value);
+    media += numeros[i];
+  }
+
+  media = media / numeros.length;
+
+  if (Number.isNaN(media) == true) {
+    caixaResultado.innerHTML = `<p id="resultado">Erro. Preencha todos os campos.</p>`;
+  } else if (Number.isInteger(media) == true) {
+    caixaResultado.innerHTML = `<p id="resultado">O resultado é: <b>${media}</b></p>`;
+  } else {
+    caixaResultado.innerHTML = `<p id="resultado">O resultado é: <b>${media.toFixed(
+      3
+    )}</b></p>`;
+  }
+}
